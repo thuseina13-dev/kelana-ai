7b1962f7caadb3c174b73486d28ceb577747fc07
thuseina13 <thuseina13@gmail.com>
2026-08-10 17:26:02 +0700

    feat: tugas session 1
    fix: fixing type

--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,21 @@
 destination = input("Destination: ")
+country = input("Country: ")
 days = int(input("Days: "))
+currency = input("Currency: ")
 budget = float(input("Budget: "))
-travel_style = input("Travel Style: "  )
-hotel_cost = float(input('Hotel Cost: '))
-transportation_cost = float(input('Transportation Cost: '))
-food_cost = float(input('Food Cost: '))
-miscellaneous_cost = float(input('Miscellaneous Cost: '))
-total_cost = hotel_cost + transportation_cost + food_cost + miscellaneous_cost
+travel_month = input("Travel Month: ")
 
-def print_trip_summary(
-        destination,
-        days, 
-        budget,
-        travel_style,
-        total_cost, 
-        hotel_cost,
-        food_cost,
-        transportation_cost,
-        miscellaneous_cost):
+def print_trip_summary(destination, country, days, budget, currency, travel_month ):
     
     print("============================")
     print('Kelana Ai')
     print("============================")
     print(f"Destination     : {destination}")    
+    print(f"Country         : {country}")    
     print(f"Days            : {days}")
-    print(f"Travel Style    : {travel_style}")
-    print(f"Budget          : {budget}")
-    print(f"Hotel Cost      : {hotel_cost}")
-    print(f"Food Cost       : {food_cost}")
-    print(f"Transportation Cost : {transportation_cost}")
-    print(f"Miscellaneous Cost : {miscellaneous_cost}")
-    print(f"Total Cost      : {total_cost}")
-
-    if(total_cost > budget):
-        print("⚠ Budget exceeded.")
-        
+    print(f"Budget          : {budget} {currency}")
+    print(f"Currency        : {currency}")
+    print(f"Travel Month    : {travel_month}")
 
 
-print_trip_summary(destination, days, budget, travel_style, total_cost, hotel_cost, food_cost, transportation_cost, miscellaneous_cost)
+print_trip_summary(destination, country, days, budget, currency, travel_month)
